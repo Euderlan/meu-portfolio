a929eb8942b342243ede0c9d3ae4f335ca255ade
Euderlan <euderlanfreire@gmail.com>
2024-05-23 15:39:41 -0300

    todos os efeitos

--- js/efeitoDigitar.js
+++ js/efeitoDigitar.js
@@ -1,14 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(){
-    var texto = "Euderlan Freire"
     var atraso = 200
     var textoElemento = document.querySelector(".titulo-destaque")
 
     var contadorDeLetras = 0
     function escreverTexto(){
-        if (contadorDeLetras < texto.length) {
-            textoElemento.textContent += texto.charAt(contadorDeLetras)
+        if (contadorDeLetras <  getTextoAtual().length) {
+            textoElemento.textContent +=  getTextoAtual().charAt(contadorDeLetras)
             contadorDeLetras++
             setTimeout(escreverTexto, atraso)
+        }
+    }
+    function getTextoAtual() {
+        
+        if (window.location.href.includes("index.html")) {
+            return "Euderlan Freire";
+        } else if (window.location.href.includes("skills.html")) {
+            return "Habilidades"; 
+        } else if (window.location.href.includes("project.html")) {
+            return "Projetos"; 
+        }else {
+            return "Texto padrão";
         }
     }
     escreverTexto()
